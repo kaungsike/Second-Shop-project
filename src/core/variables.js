f4f0109e34c2fa3b03af5c691d6222b00dabb8a7
kaungsike <kaungsike9@gmail.com>
2024-05-09 11:14:50 +0630

    UI updated

---
+++ src/core/variables.js
@@ -0,0 +1,13 @@
+export const products = [
+    {
+        id : 1,
+        name : "Nike Air Force 1",
+        gander : "man",
+        img : "https://images.footlocker.com/is/image/EBFL2/W2288111?wid=267&hei=267&fmt=png-alpha",
+        price : 115.78,
+        rating : {
+            rate : 4.6,
+            count : 308
+        }
+    }
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ export default {
     fontFamily : {
       sans :  ['Open Sans', 'sans-serif']
     },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [require('preline/plugin')],
