9198b52c9c02ce2c04e2a0b4d7b3884a24d42165
kaungsike <kaungsike9@gmail.com>
2024-05-09 11:36:02 +0630

    added 10  variables

--- src/core/variables.js
+++ src/core/variables.js
@@ -1,13 +1,101 @@
 export const products = [
     {
         id : 1,
-        name : "Nike Air Force 1",
-        gander : "man",
+        name : "Nike Air Force 1 '07 LE",
+        brand : "nike",
         img : "https://images.footlocker.com/is/image/EBFL2/W2288111?wid=267&hei=267&fmt=png-alpha",
-        price : 115.78,
-        rating : {
-            rate : 4.6,
-            count : 308
-        }
-    }
-]
+        price : 115.75,
+        rating : 4.6,
+    },
+    {
+        id : 2,
+        name : "Nike Air Force 1 Low",
+        brand : "nike",
+        img : "https://images.footlocker.com/is/image/EBFL2/D2920111?wid=200&hei=200&fmt=png-alpha",
+        price : 98.05,
+        rating : 4.7,
+
+    }, 
+    {
+        id :3,
+        name : "Nike Air Force 1 '07 LE Low",
+        brand : "nike",
+        img : "https://images.footlocker.com/is/image/EBFL2/D8959100?wid=200&hei=200&fmt=png-alpha",
+        price : 115.75,
+        rating : 4.6,
+
+    }, 
+    {
+        id :4,
+        name : "Nike Air Force 1 Shadow",
+        brand : "nike",
+        img : "https://images.footlocker.com/is/image/EBFL2/I0919100?wid=200&hei=200&fmt=png-alpha",
+        price : 134.99,
+        rating : 4.8,
+
+    }, 
+    {
+        id :5,
+        name : "Nike Air Force 1 '07 FL",
+        brand : "nike",
+        img : "https://images.footlocker.com/is/image/EBFL2/Z4627400?wid=200&hei=200&fmt=png-alpha",
+        price : 134.99,
+        rating : 4.7,
+
+    }, 
+    {
+        id :6,
+        name : "Nike Air Force 1 '07 ESS",
+        brand : "nike",
+        img : "https://images.footlocker.com/is/image/EBFL2/F4146101?wid=200&hei=200&fmt=png-alpha",
+        price : 112.55,
+        rating : 4.5,
+
+    }, 
+    {
+        id :7,
+        name : "Nike Air Force 1 '07 FL",
+        brand : "nike",
+        img : "https://images.footlocker.com/is/image/EBFL2/Z4627800?wid=200&hei=200&fmt=png-alpha",
+        price : 140.00,
+        rating : 4.8,
+
+    }, 
+    {
+        id :8,
+        name : "Nike Air Force 1 Shadow CE",
+        brand : "nike",
+        img : "https://images.footlocker.com/is/image/EBFL2/Q8885100?wid=200&hei=200&fmt=png-alpha",
+        price : 135.00,
+        rating : 3.9,
+
+    }, 
+    {
+        id :9,
+        name : "Nike Air Force 1 '07 LV8 EMB",
+        brand : "nike",
+        img : "https://images.footlocker.com/is/image/EBFL2/F4833001?wid=200&hei=200&fmt=png-alpha",
+        price : 150.00,
+        rating : 4.7,
+
+    }, 
+    {
+        id :10,
+        name : "Nike Air Force 1 High '07 LE",
+        brand : "nike",
+        img : "https://images.footlocker.com/is/image/EBFL2/W2290111?wid=200&hei=200&fmt=png-alpha",
+        price : 112.99,
+        rating : 3.6,
+
+    }, 
+    // {
+    //     id :,
+    //     name : "",
+    //     gander : "",
+    //     img : "",
+    //     price : ,
+    //     rating : ,
+
+    // }, 
+
+  ]
